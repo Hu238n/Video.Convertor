dbc6a3174216b28cbbad683cf96c625a4ab16194
Hussein Al Rekabei <30638498+Hu238n@users.noreply.github.com>
2021-12-08 07:21:07 +0300

    Add project files.

---
+++ Video.Convertor/FFmpegTask.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+
+namespace Video.Convertor
+{
+    public class FFmpegTask
+    {
+        public Process Process = new Process();
+
+        public FFmpegTask(string ffmpegPath, string arguments)
+        {
+            Process.StartInfo.FileName = ffmpegPath;
+            Process.StartInfo.Arguments = arguments;
+            Process.StartInfo.RedirectStandardOutput = true;
+            Process.StartInfo.CreateNoWindow = false;
+            Process.StartInfo.UseShellExecute = false;
+        }
+        public bool Start()
+        {
+            return Process.Start();
+        }
+    }
+}
---
+++ Video.Convertor/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Video.Convertor;
+const string videoPath = @"InputVideoFIlePath";
+const string saveIn = @"OutPutVideoFilePath";
+const string ffmpegArg = $"-i \"{videoPath}\" -vf scale=-1:720 \"{saveIn}\"";
+const string ffmpegPath = @"C:\FilePath\ffmpeg.exe";
+var ffmpegTask = new FFmpegTask(ffmpegPath, ffmpegArg);
+ffmpegTask.Start();
